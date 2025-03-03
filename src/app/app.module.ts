829af2819337c155d997c01d11bade7ebd235981
jessicagomestec <jessicagomestecnico@gmail.com>
2025-03-03 15:58:40 -0300

    big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { CheckSampleComponent } from './check-sample/check-sample.component';
-import { CardComponent } from './card/card.component';
-import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
-import {FormsModule}from '@angular/forms'
-import { ListaModule} from './lista/lista.module';
+
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
@@ -17,10 +12,7 @@ import { HomeComponent } from './pages/home/home.component'
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent,
-    CheckSampleComponent,
-    CardComponent,
-    CompAtributosComponent,
+
     MenuBarComponent,
     MenuTitleComponent,
     SmallCardComponent,
@@ -29,9 +21,8 @@ import { HomeComponent } from './pages/home/home.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ListaModule
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
