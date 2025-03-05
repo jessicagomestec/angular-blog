1c38899781d8a01b489e9a42a76375c77aacfbe9
jessicagomestec <jessicagomestecnico@gmail.com>
2025-03-05 18:10:01 -0300

    update card components with new styles and content; enhance footer and home page with relevant images and titles

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,50 +1,50 @@
 export const dataFake = [
   {
     "id":"1",
-    "title":"formas dinamicas",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":"./assets/placeholder.png"
+    "title":"Comportamento canino: o que significa cachorro arranhando o chão?",
+    "description":"Quem disse que cachorro não fala? Embora a voz seja o principal meio de comunicação […]",
+    "photoCover":"./assets/img/cachorro-arranhando-o-chao-caa.webp"
   },
   {
     "id":"2",
-    "title":"formas 2",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":"./assets/placeholder.png"
+    "title":"Caudectomia e mais procedimentos proibidos em cães",
+    "description":"Apesar de já ter sido uma prática popular há décadas, a caudectomia é proibida desde […]",
+    "photoCover":"./assets/img/caudectomia1.webp"
   },
   {
     "id":"3",
-    "title":"formas 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Quais são os animais que botam ovo?",
+    "description":"quais são os animais que botam ovo, Existem milhares de animais, dos mais diversos tipos, espécies e formas. Alguns são aquáticos, outros terrestres e outros semiaquáticos. Mas afinal, ",
+    "photoCover":"./assets/img/quais-sao-os-animais-que-botam-ovo-final.webp"
   },
   {
     "id":"4",
-    "title":"formas 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Nomes para gatos: ideias incríveis para se inspirar!",
+    "description":"Ao decidir adotar um gato, é normal ficar muito empolgado, mas indeciso na hora de batizá-lo. Isso porque o tutor quer sempre dar o apelido perfeito para seu novo pet. Para ajudar, abaixo você encontra algumas ideias de ",
+    "photoCover":"./assets/img/nomes-para-gatos-3.webp"
   },
   {
     "id":"5",
-    "title":"footer 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Calopsita com piolho? Veja o que fazer e como identificar",
+    "description":"As calopsitas são pets amados, muito populares nos lares brasileiros. Não à toa, a saúde […]",
+    "photoCover":"./assets/img/coelho_03-1024x639.webp"
   },
   {
     "id":"6",
-    "title":"footer 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Como saber se o coelho é macho ou fêmea?",
+    "description":"Adotar um coelho de estimação é um momento importante para a família. Há quem prefira […]",
+    "photoCover":"./assets/img/coelho_03-1024x639.webp"
   },
   {
     "id":"7",
-    "title":"footer 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Nomes para cachorro: escolha o que mais combina com seu peludo!",
+    "description":"Acabou de chegar um novo amigo na sua casa e você ainda não sabe como chamá-lo? Então, está no lugar certo! Escolher ",
+    "photoCover":"./assets/img/nomes-para-cachorro-interna2.webp"
   },
   {
     "id":"8",
-    "title":"footer 3",
-    "description":"adicionar tudo dinamicamente",
-    "photoCover":""
+    "title":"Conheça animais de aquário doméstico para ter em casa!",
+    "description":"Se você tem um aquário em casa e gostaria de ter mais habitantes além dos […]",
+    "photoCover":"./assets/img/animais-de-aquario-domestico-capa.webp"
   },
 ]
